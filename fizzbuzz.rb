ecf8d5df20a2c9d4f9e08aae57a7ffa1c1d439ce
Mariam Jaludi <mariam.jaludi@gmail.com>
2019-02-01 14:08:49 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,11 +3,11 @@
 
 def fizzbuzz(num)
   if num % 15 == 0
-    return "fizzbuzz"
+    return "Fizzbuzz"
   elsif num % 3 == 0
-    return "fizz"
+    return "Fizz"
   elsif num % 5 == 0
-    return "buzz"
+    return "Buzz"
   else return nil
   end
 end
